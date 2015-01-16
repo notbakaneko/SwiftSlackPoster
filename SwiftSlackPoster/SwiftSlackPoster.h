ef047fe921ca9ac99c7bef28a32ba12debd1780c
bakaneko <bakaneko@pixiv.com>
2015-01-16 12:08:18 +0900

    Initial Commit

---
+++ SwiftSlackPoster/SwiftSlackPoster.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSlackPoster.h
+//  SwiftSlackPoster
+//
+//  Created by bakaneko on 16/01/2015.
+//  Copyright (c) 2015 nekonyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSlackPoster.
+FOUNDATION_EXPORT double SwiftSlackPosterVersionNumber;
+
+//! Project version string for SwiftSlackPoster.
+FOUNDATION_EXPORT const unsigned char SwiftSlackPosterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSlackPoster/PublicHeader.h>
+
+
